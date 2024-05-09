33ae70687affe4608e52d94f0773604858d4d042
matheus3881 <167131896+matheus3881@users.noreply.github.com>
2024-05-08 22:21:21 -0300

    navbar responsivo
    adicionei linhas no css, html e js.

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -1,2 +1,11 @@
 // este codigo é usado para criar um carrosel na pag
-$('#area-postagem').slick();
+// $('#area-postagem').slick();
+
+
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu")
+
+hamburguer.addEventListener("click", () =>{
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
