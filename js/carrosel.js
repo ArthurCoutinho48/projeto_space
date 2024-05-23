da3c12ad9c4ce0820578f582eebf576dc00349b2
matheus3881 <167131896+matheus3881@users.noreply.github.com>
2024-05-22 23:53:20 -0300

    adição carrosel com slick Slider
    adicionei cod no index.html, styleHome.css e uma novo arquivo js( carrosel.js)

---
+++ js/carrosel.js
@@ -0,0 +1,11 @@
+
+$('.carrosel').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
