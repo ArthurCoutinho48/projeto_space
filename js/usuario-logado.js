3914a6e6fd9b776ea249a9e240e19122f708baff
Lucssv <silva.lucas96@gmail.com>
2024-06-14 22:11:01 -0300

    usuario logado

---
+++ js/usuario-logado.js
@@ -0,0 +1,4 @@
+var nomeUsuario = "Nome do Usuário"; // Substitua isso pelo método que você usa para obter o nome do usuário
+
+// Agora, você pode definir este nome de usuário em algum elemento HTML
+document.getElementById("nomeUsuario").textContent=nomeUsuario;
