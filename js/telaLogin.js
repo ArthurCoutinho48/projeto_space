6daa76a4ac1e386baf8ca057199dd07817edb31f
matheus3881 <167131896+matheus3881@users.noreply.github.com>
2024-06-16 11:51:53 -0300

    atualização

--- js/telaLogin.js
+++ js/telaLogin.js
@@ -11,7 +11,7 @@ function conferirUsuario(event){
        
 
         setTimeout(() =>{
-             location.href = 'http://127.0.0.1:5502/cadastro.html'
+             location.href = 'http://127.0.0.1:5502/index.html'
         }, 1000)
 
         let token = Math.random().toString(16).substring(2) 
